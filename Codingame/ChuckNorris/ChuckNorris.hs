536a1ea3e668f64818acb37aed07422d12d71be8
Martin Heuschober <epsilonhalbe@gmail.com>
2014-01-01 19:31:04 +0100

    Fixed:  error in the implementation of ChuckNorris

--- Codingame/ChuckNorris/ChuckNorris.hs
+++ Codingame/ChuckNorris/ChuckNorris.hs
@@ -1,15 +1,6 @@
-{-# OPTIONS_GHC
-  -fno-warn-unused-binds
-  -fno-warn-unused-matches
-#-}
-
 module Main where
 
 import Control.Applicative ((<$>))
---import Control.Arrow
---import qualified Data.ByteString.Lazy as B
---import qualified Prelude as P
---import Data.Word
 import Data.Char
 import Data.Bits
 import Data.List (group)
@@ -21,10 +12,13 @@ instance Show Bit where
   show (Bit False) = "00"
 
 main :: IO ()
-main = do input <- group . concatMap (reverse . bitsToList . ord) <$> getLine ::IO [[Bit]]
+main = do input <- group . concatMap (reverse . bitsTo7Bit . ord) <$> getLine ::IO [[Bit]]
           let output = concatMap (\x-> [show $ head x, replicate (length x) '0']) input
           putStrLn $ unwords output
           return ()
+
+bitsTo7Bit :: Bits a => a -> [Bit]
+bitsTo7Bit x = take 7 $ bitsToList x ++ repeat (Bit False)
 
 bitsToList :: Bits a => a -> [Bit]
 bitsToList x = if x == zero then []
@@ -32,7 +26,3 @@ bitsToList x = if x == zero then []
 
 zero :: Bits a => a
 zero = bit 1 `xor` bit 1
-
--- helper stuff
-(∈) :: (Eq a) => a -> [a] -> Bool
-(∈) = elem
